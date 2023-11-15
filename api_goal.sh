edb46b217c2355f1065c528ea80bccfc0b04af8d
Aleksandra Modrzejewska <114804356+xAleksandrax@users.noreply.github.com>
2023-11-15 09:26:39 +0100

    Update api_goal.sh

--- api_goal.sh
+++ api_goal.sh
@@ -1,8 +1,7 @@
 BASE_URL="http://localhost:8000"
-goal_id="1"
 
-curl -X GET "${BASE_URL}/goals/${goal_id}"
+curl -X GET "${BASE_URL}/goals/1"
 curl -X GET "${BASE_URL}/goals/"
-curl -X POST "${BASE_URL}/goals" -H "Content-Type: application/json" -d '{"id": "${goal_id}", "description": "HELLO THERE"}'
-curl -X PUT "${BASE_URL}/goals/${goal_id}" -H "Content-Type: application/json" -d '{"description": "WELCOME"}'
-curl -X DELETE "${BASE_URL}/goals/${goal_id}"
+curl -X POST "${BASE_URL}/goals" -H "Content-Type: application/json" -d '{"description": "HELLO THERE"}'
+curl -X PUT "${BASE_URL}/goals/1" -H "Content-Type: application/json" -d '{"description": "WELCOME"}'
+curl -X DELETE "${BASE_URL}/goals/1"
